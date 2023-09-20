65077ce593e44a21d420e82ca665c065ee4b0469
alessandrosaladino-tech <alessandro.saladino22@gmail.com>
2023-09-20 15:48:44 +0200

    Add
    Some comments to understand what i'm doing

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,20 +11,20 @@ createApp({
     data() {
         return {
 
-            emails: [],
+            emails: [], //Create ab array to contain all the emails we receive from the API
 
         }
     },
 
     mounted() {
-        for (let index = 0; index < 10; index++) {
+        for (let index = 0; index < 10; index++) { //Repeat the API 10 times
 
 
 
-            axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+            axios.get("https://flynn.boolean.careers/exercises/api/random/mail") //Getting API for random email
                 .then(response => {
                     console.log("ciao");
-                    this.emails.push(response.data.response);
+                    this.emails.push(response.data.response); //Push into the array the response of the API
                     console.log(this.emails);
                 })
         }
